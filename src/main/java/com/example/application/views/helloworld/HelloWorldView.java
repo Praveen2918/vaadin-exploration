fa6946e7de8a4791cda9896b009e1a8e1b846531
start.vaadin.com <no-reply@vaadin.com>
2022-02-01 11:24:26 +0000

    Generated project

---
+++ src/main/java/com/example/application/views/helloworld/HelloWorldView.java
@@ -0,0 +1,24 @@
+package com.example.application.views.helloworld;
+
+import com.example.application.components.leafletmap.LeafletMap;
+import com.example.application.views.MainLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+
+@PageTitle("Hello World")
+@Route(value = "hello", layout = MainLayout.class)
+@RouteAlias(value = "", layout = MainLayout.class)
+public class HelloWorldView extends VerticalLayout {
+
+    private LeafletMap map = new LeafletMap();
+
+    public HelloWorldView() {
+        setSizeFull();
+        setPadding(false);
+        map.setSizeFull();
+        map.setView(55.0, 10.0, 4);
+        add(map);
+    }
+}
